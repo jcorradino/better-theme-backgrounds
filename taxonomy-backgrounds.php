749cc171904f29bfde10215a2a9e52301f1f0b90
Jason Corradino <jason@ididntbreak.it>
2013-02-22 13:45:19 -0600

    initial plugin commit

---
+++ taxonomy-backgrounds.php
@@ -0,0 +1,58 @@
+<?php
+/*
+Plugin Name: Better Theme Backgrounds
+Description: Replaces current wordpress custom background page with a better page that allows customization based on taxonomy
+Version: 0.1-alpha
+Author: Jason Corradino
+Author URI: http://imyourdeveloper.com
+License: GPL2
+
+Copyright 2012  Jason Corradino  (email : Jason@ididntbreak.it)
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License, version 2, as 
+published by the Free Software Foundation.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+function _custom_header_background_just_in_time_taxonomy_background_override() {
+	global $custom_image_header, $custom_background;
+
+	if ( current_theme_supports( 'custom-header' ) ) {
+		// In case any constants were defined after an add_custom_image_header() call, re-run.
+		add_theme_support( 'custom-header', array( '__jit' => true ) );
+
+		$args = get_theme_support( 'custom-header' );
+		if ( $args[0]['wp-head-callback'] )
+			add_action( 'wp_head', $args[0]['wp-head-callback'] );
+
+		if ( is_admin() ) {
+			require_once( ABSPATH . 'wp-admin/custom-header.php' );
+			$custom_image_header = new Custom_Image_Header( $args[0]['admin-head-callback'], $args[0]['admin-preview-callback'] );
+		}
+	}
+
+	if ( current_theme_supports( 'custom-background' ) ) {
+		// In case any constants were defined after an add_custom_background() call, re-run.
+		add_theme_support( 'custom-background', array( '__jit' => true ) );
+
+		$args = get_theme_support( 'custom-background' );
+		add_action( 'wp_head', $args[0]['wp-head-callback'] );
+
+		if ( is_admin() ) {
+			require_once( plugin_dir_path(__FILE__) . 'includes/background-class.php' );
+			$custom_background = new Tax_Custom_Background( $args[0]['admin-head-callback'], $args[0]['admin-preview-callback'] );
+		}
+	}
+}
+
+remove_action( 'wp_loaded', '_custom_header_background_just_in_time' );
+add_action( 'wp_loaded', '_custom_header_background_just_in_time_taxonomy_background_override' );
